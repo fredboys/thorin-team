195755a61af71b58c592aaf5beb9bc5b45a5f6bf
fredboys <freddieboys@hotmail.co.uk>
2022-10-27 16:35:43 +0000

    Fix whitespace

--- run.py
+++ run.py
@@ -29,6 +29,7 @@ def about_member(member_name):
                 member = obj
     return render_template("member.html", member=member)
 
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html", page_title="Contact")
